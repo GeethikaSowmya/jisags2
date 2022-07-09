9e8bc673911d59391b2bb83acad3fb17978ac36b
Your Name <Geethika@Developer>
2022-07-09 22:02:44 +0530

    dto implementation

---
+++ src/main/java/com/example/demo/model/Address.java
@@ -0,0 +1,75 @@
+package com.example.demo.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "state")
+	private String state ;
+
+	@Column(name = "city")
+	private String city;
+	
+	@Column(name = "postalcode")
+	private String postalcode;
+
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "address")
+	private Users users;
+
+	public Address() {
+
+	}
+
+	public Address(String state, String city, String postalcode) {
+		super();
+		this.state = state;
+		this.city = city;
+		this.postalcode = postalcode;
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPostalcode() {
+		return postalcode;
+	}
+
+	public void setPostalcode(String postalcode) {
+		this.postalcode = postalcode;
+	}
+
+	public Users getUsers() {
+		return users;
+	}
+
+	public void setUsers(Users users) {
+		this.users = users;
+	}
+}
